8d783af48f1b0a2cf7fd6f682c001d83a7bf42da
AndrejBlinov <andrej.blinov.1991@mail.ru>
2022-02-09 14:55:33 +0300

    bf

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  const PORT = process.env.APP_PORT || 8080;
-  await app.listen(PORT, function() {
-    console.log( `im starting in port ${PORT}`)
-  });
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
