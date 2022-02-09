0ddc11594b408ef150264c876505e346d70aeab8
AndrejBlinov <andrej.blinov.1991@mail.ru>
2022-02-09 12:54:48 +0300

    bf

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);;
   
   const PORT = process.env.APP_PORT || 8080;
   await app.listen(PORT, function() {
